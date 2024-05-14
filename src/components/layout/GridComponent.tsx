b37ad56243c58326e9e0659e96dfce18f5d9b3ac
DeclawedLyon <declan.e.lyons@gmail.com>
2024-05-13 21:53:52 -0700

    creates <Grid/>

---
+++ src/components/layout/GridComponent.tsx
@@ -0,0 +1,17 @@
+// GridComponent.tsx
+import React from 'react';
+
+interface GridComponentProps {
+  children: React.ReactNode;
+  columns?: number; // Optional prop to set the number of columns
+}
+
+const GridComponent: React.FC<GridComponentProps> = ({ children, columns = 3 }) => {
+  return (
+    <div className={`grid grid-cols-${columns} gap-4`}>
+      {children}
+    </div>
+  );
+};
+
+export default GridComponent;
